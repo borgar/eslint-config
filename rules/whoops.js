609dc9257ff9b61701155717274912216b9cc4ee
Borgar <borgar@borgar.net>
2021-04-14 16:54:23 +0000

    Update a broken rule, dependencies, and bump version

--- rules/whoops.js
+++ rules/whoops.js
@@ -32,7 +32,7 @@ module.exports = {
     'no-unmodified-loop-condition': 'error',
     'no-unreachable': 'error',
     'no-unreachable-loop': 'error',
-    'no-unused-vars': [ 'error', { vars: 'all', args: 'none' } ],
+    'no-unused-vars': 'error',
     'no-useless-backreference': 'warn',
     'no-var': 'warn',
     'prefer-numeric-literals': 'error',
